232b01262eb1399660eba91d0c72c617218bd5e1
smit <smitjogani00@gmail.com>
2024-05-30 15:17:43 +0530

    Portfolio

---
+++ app/page.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { navItems } from "@/data";
+
+import Hero from "@/components/Hero";
+import Grid from "@/components/Grid";
+import Footer from "@/components/Footer";
+import Experience from "@/components/Experience";
+import RecentProjects from "@/components/RecentProjects";
+import { FloatingNav } from "@/components/ui/FloatingNavbar";
+import Clients from "@/components/Clients";
+
+const Home = () => {
+  return (
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <FloatingNav navItems={navItems} />
+        <Hero />
+        <Grid />
+        <RecentProjects />
+        <Experience />
+        <Clients/>
+        <Footer />
+      </div>
+    </main>
+  );
+};
+
+export default Home;
---
+++ data/index.ts
@@ -0,0 +1,256 @@
+export const navItems = [
+  { name: "Home", link: "#home" },
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#project" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a JS Animation library",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "House Craft",
+    des: "Provide Interior Designerss and Architecture and provide dashboard them for analysis.",
+    img: "/hc.png",
+    iconLists: ["/re.svg", "/node.svg", "/mongo.svg", "/ex.svg"],
+    link: "https://github.com/smitjogani/HouseCraft_frontend",
+  },
+  {
+    id: 2,
+    title: "Gbook - Cricket Turf and Ground Booking System",
+    des: "Online Cricket Turf and Ground Booking System and owner also manage dynamically.",
+    img: "/gbook.png",
+    iconLists: ["/java.svg", "/mysql.svg"],
+    link: "https://github.com/smitjogani/Ground-Booking",
+  },
+  {
+    id: 3,
+    title: "Cloth Garrage - Online Cloth Shop",
+    des: "E-cloth store which is provide online plateform for cloth store.",
+    img: "/ec.png",
+    iconLists: ["/angular.svg", "/node.svg", "/mongo.svg", "/ex.svg"],
+    link: "https://github.com/smitjogani/Ecommerce---MEAN-Technology",
+  },
+  {
+    id: 4,
+    title: "Animated Apple Iphone 3D Website",
+    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+    img: "/ios.png",
+    iconLists: ["/re.svg", "/tail.svg", "/three.svg", "/gsap.svg", "/fm.svg"],
+    link: "https://github.com/smitjogani/Apple-iPhone15-UI",
+  },
+  {
+    id: 5,
+    title: "Ochi(clone)- A digital Agency",
+    des: "Online PLateform For Digital Agency",
+    img: "/ochi.png",
+    iconLists: ["/re.svg", "/tail.svg", "/gsap.svg", "/fm.svg"],
+    link: "https://github.com/smitjogani/Ochi-UI-Clone",
+  },
+  {
+    id: 6,
+    title: "Rejouce - The Venture Agency",
+    des: "Degital Presentance of Rejouce-The Venture Agency.",
+    img: "/rejouce.png",
+    iconLists: ["/re.svg", "/gsap.svg", "/fm.svg"],
+    link: "https://github.com/smitjogani/Rejouce-ui",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "ReactJS",
+    img: "/re.svg",
+  },
+  {
+    id: 2,
+    name: "NodeJS",
+    img: "/node.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "ExpressJS",
+    img: "/ex.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "MongoDB",
+    img: "/mongo.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "Java",
+    img: "/java.svg",
+    nameImg: "/dockerName.svg",
+  },
+  {
+    id: 6,
+    name: "Python",
+    img: "/py.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 7,
+    name: "Mysql",
+    img: "/mysql.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 8,
+    name: "JavaScript",
+    img: "/js.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 9,
+    name: "Angular",
+    img: "/angular.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 10,
+    name: "Github",
+    img: "/git.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Website Development",
+    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Application Development",
+    desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+    className: "md:col-span-2",
+    thumbnail: "/exp2.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/smitjogani",
+  },
+  {
+    id: 2,
+    img: "/cn.svg",
+    link: "https://www.naukri.com/code360/profile/smit2020",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/smit-jogani-4b0277228/",
+  },
+  {
+    id: 4,
+    img: "/leetcode.svg",
+    link: "https://leetcode.com/u/Smit_Jogani/",
+  },
+];
